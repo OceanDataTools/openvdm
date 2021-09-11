a1bdcf1bc0c29af8e567baecb886f9a182b36264
Webb Pinner <webbpinner@gmail.com>
2021-09-11 09:29:06 -0400

    searching

--- www/app/templates/default/js/welcome.js
+++ www/app/templates/default/js/welcome.js
@@ -249,9 +249,9 @@ $(function () {
                             }
                             shipToShoreTransfersOutput += '                       <li><small>' + data[i].newFiles[j] + '</small></li>';
                         }
-                        if(data[i].newFiles.length > 40){
-                            shipToShoreTransfersOutput += '                       <li><small>... and <strong>' + data[i].newFiles.length-40 + '</strong> other files</small></li>';
-                        }
+                        // if(data[i].newFiles.length > 40){
+                        //     shipToShoreTransfersOutput += '                       <li><small>... and <strong>' + data[i].newFiles.length-40 + '</strong> other files</small></li>';
+                        // }
                         if (data[i].updatedFiles.length > 0) {
                             shipToShoreTransfersOutput += '                       <li><small>' + data[i].updatedFiles.length + ' File(s) Updated</small></li>';
                         }
