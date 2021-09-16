b3746eb20ec964f1a1a0eebaaa277f4436dfddd1
Webb Pinner <webbpinner@gmail.com>
2021-09-16 12:03:00 -0400

    hopfully fixed the parsing issue

--- server/workers/run_ship_to_shore_transfer.py
+++ server/workers/run_ship_to_shore_transfer.py
@@ -162,13 +162,13 @@ def transfer_ssh_dest_dir(gearman_worker, gearman_job):
         if line.startswith( '<f+++++++++' ):
             filename = line.split(' ',1)[1]
             files['new'].append(filename)
-            logging.info("Progress Update: %d/%d", file_index, file_count)
+            logging.info("Progress Update: %d%%", int(100 * (file_index + 1)/file_count))
             gearman_worker.send_job_status(gearman_job, int(20 + 70*float(file_index)/float(file_count)), 100)
             file_index += 1
         elif line.startswith( '<f.' ):
             filename = line.split(' ',1)[1]
             files['updated'].append(filename)
-            logging.info("Progress Update: %d/%d", file_index, file_count)
+            logging.info("Progress Update: %d%%", int(100 * (file_index + 1)/file_count))
             gearman_worker.send_job_status(gearman_job, int(20 + 70*float(file_index)/float(file_count)), 100)
             file_index += 1
 
