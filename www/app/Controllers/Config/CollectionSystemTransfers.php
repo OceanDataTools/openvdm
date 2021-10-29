6afac5b888509299e7cc0c8ce31d7ae70084751c
Webb Pinner <webbpinner@gmail.com>
2021-10-29 13:21:14 -0400

    syntax error

--- www/app/Controllers/Config/CollectionSystemTransfers.php
+++ www/app/Controllers/Config/CollectionSystemTransfers.php
@@ -554,6 +554,7 @@ class CollectionSystemTransfers extends Controller {
 
         $data['row'] = $this->_collectionSystemTransfersModel->getCollectionSystemTransfer($id);        
 
+        var_dump($data['row'][0]);
         $data['stalenessOptions'] = $this->_buildStalenessOptions($data['row'][0]['staleness']);
         
         if(isset($_POST['submit'])){
