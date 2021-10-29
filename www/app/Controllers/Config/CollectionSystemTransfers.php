84fa017a5bd528882372df64a1220bb16e1b7ac2
Webb Pinner <webbpinner@gmail.com>
2021-10-29 13:23:49 -0400

    debugging

--- www/app/Controllers/Config/CollectionSystemTransfers.php
+++ www/app/Controllers/Config/CollectionSystemTransfers.php
@@ -554,8 +554,8 @@ class CollectionSystemTransfers extends Controller {
 
         $data['row'] = $this->_collectionSystemTransfersModel->getCollectionSystemTransfer($id);        
 
-        var_dump($data['row'][0]['staleness']);
-        $data['stalenessOptions'] = $this->_buildStalenessOptions($data['row'][0]['staleness']);
+        var_dump($data['row'][0]->staleness);
+        $data['stalenessOptions'] = $this->_buildStalenessOptions($data['row'][0]]->staleness);
         
         if(isset($_POST['submit'])){
             $name = $_POST['name'];
