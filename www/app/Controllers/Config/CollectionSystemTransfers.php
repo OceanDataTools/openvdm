0d1c764f059d4d0a1a706549a5e8d0cf0bbba9cf
Webb Pinner <webbpinner@gmail.com>
2021-10-29 13:19:26 -0400

    syntax error

--- www/app/Controllers/Config/CollectionSystemTransfers.php
+++ www/app/Controllers/Config/CollectionSystemTransfers.php
@@ -554,7 +554,7 @@ class CollectionSystemTransfers extends Controller {
 
         $data['row'] = $this->_collectionSystemTransfersModel->getCollectionSystemTransfer($id);        
 
-        $data['stalenessOptions'] = $this->_buildStalenessOptions(data['row'][0]['staleness']);
+        $data['stalenessOptions'] = $this->_buildStalenessOptions($data['row'][0]['staleness']);
         
         if(isset($_POST['submit'])){
             $name = $_POST['name'];
