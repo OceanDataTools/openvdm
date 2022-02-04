f61b41dbf432e81a1ffa1b3c0493f238376bb804
Webb Pinner <webbpinner@gmail.com>
2022-02-04 12:43:57 -0500

    added br

--- www/app/views/Config/editCruiseDataTransfers.php
+++ www/app/views/Config/editCruiseDataTransfers.php
@@ -58,19 +58,19 @@ $data['row'][0]->excludedExtraDirectories = explode(',', $data['row'][0]->exclud
 <?php
     if ($data['collectionSystemTransfers']) {
 ?>
-                                <!-- <div class="form-group"> -->
+                                <div class="form-group">
                                     <label for='excludedCollectionSystems[]'>Select any Collection Systems to EXCLUDE:</label><br>
                                     <div class="checkbox">
 <?php
         foreach ($data['collectionSystemTransfers'] as $key => $value) {
 ?>
                                         <input type="checkbox" name="excludedCollectionSystems[]" value=<?php echo $value->collectionSystemTransferID;?> <?php echo (in_array($value->collectionSystemTransferID, $data['row'][0]->excludedCollectionSystems))? 'checked': '' ?>>
-                                        <label><?php echo $value->longName;?></label>
+                                        <label><?php echo $value->longName;?></label></br>
 <?php
         }
 ?>  
                                     </div>
-                                <!-- </div> -->
+                                </div>
 <?php
     } else {
 ?>
