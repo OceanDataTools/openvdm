a7857ee1434595ac9f26e5cfd8eae999b33acda0
Webb Pinner <webbpinner@gmail.com>
2022-02-07 08:36:36 -0500

    style tweaks

--- www/app/templates/default/js/dataDashboardMain.js
+++ www/app/templates/default/js/dataDashboardMain.js
@@ -47,6 +47,7 @@ $(function () {
                             label: data[i].label + ' (' + data[i].unit + ')',
                             yAxisID: data[i].label,
                             borderColor: colors[i%colors.length],
+                            borderWidth: 1,
                             backgroundColor: colors[i%colors.length],
                         });
                                 
