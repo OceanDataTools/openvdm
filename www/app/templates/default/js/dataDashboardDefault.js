598368d653e24199ffce9704355a5685d5cb2fa3
Webb Pinner <webbpinner@gmail.com>
2022-02-07 11:26:43 -0500

    fixed it... maybe

--- www/app/templates/default/js/dataDashboardDefault.js
+++ www/app/templates/default/js/dataDashboardDefault.js
@@ -466,15 +466,8 @@ $(function () {
                                         chartObject['chart'].data.datasets[index].hidden = 
                                             !chartObject['chart'].data.datasets[index].hidden;
                                         //toggle the related labels' visibility
-                                        console.log(chartObject['chart'].data.datasets[index].yAxisID);
-                                        console.log(chartObject['chart'].options.scales);
-                                        console.log(chartObject['chart'].options.scales[chartObject['chart'].data.datasets[index].yAxisID]);
-                                        // console.log(chartObject['chart'].options.scales.yAxes);
-                                        // console.log(chartObject['chart'].options.scales.yAxes[index]);
                                         chartObject['chart'].options.scales[chartObject['chart'].data.datasets[index].yAxisID].display =                 
                                             !chartObject['chart'].options.scales[chartObject['chart'].data.datasets[index].yAxisID].display
-                                        // chartObject['chart'].options.scales.yAxes[index].display =                 
-                                        //     !chartObject['chart'].options.scales.yAxes[index].display;
                                         chartObject['chart'].update();
                                     }
                                 }
