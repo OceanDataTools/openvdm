b11c0464e22ae766c4fff9e32074b98ffee12488
Webb Pinner <webbpinner@gmail.com>
2022-03-12 12:06:13 -0500

    fixing sql migration script

--- database/openvdm_27_to_28.sql
+++ database/openvdm_27_to_28.sql
@@ -4,10 +4,10 @@
 
 LOCK TABLES `OVDM_CollectionSystemTransfers` WRITE;
 /*!40000 ALTER TABLE `OVDM_CollectionSystemTransfers` DISABLE KEYS */;
-ALTER TABLE `OVDM_CollectionSystemTransfers` ADD
-`skipEmptyDirs` int(1) unsigned NOT NULL DEFAULT '1',
-`skipEmptyFiles` int(1) unsigned NOT NULL DEFAULT '1',
-`syncFromSource` int(1) unsigned NOT NULL DEFAULT '0';
+ALTER TABLE `OVDM_CollectionSystemTransfers`
+ADD `skipEmptyDirs` int(1) unsigned NOT NULL DEFAULT '1',
+ADD `skipEmptyFiles` int(1) unsigned NOT NULL DEFAULT '1',
+ADD `syncFromSource` int(1) unsigned NOT NULL DEFAULT '0';
 
 /*!40000 ALTER TABLE `OVDM_CollectionSystemTransfers` ENABLE KEYS */;
 UNLOCK TABLES;
@@ -17,10 +17,10 @@ UNLOCK TABLES;
 
 LOCK TABLES `OVDM_CruiseDataTransfers` WRITE;
 /*!40000 ALTER TABLE `OVDM_CruiseDataTransfers` DISABLE KEYS */;
-ALTER TABLE `OVDM_CruiseDataTransfers` ADD
-`skipEmptyDirs` int(1) unsigned NOT NULL DEFAULT '1',
-`skipEmptyFiles` int(1) unsigned NOT NULL DEFAULT '1',
-`syncToDest` int(1) unsigned NOT NULL DEFAULT '0';
+ALTER TABLE `OVDM_CruiseDataTransfers`
+ADD `skipEmptyDirs` int(1) unsigned NOT NULL DEFAULT '1',
+ADD `skipEmptyFiles` int(1) unsigned NOT NULL DEFAULT '1',
+ADD `syncToDest` int(1) unsigned NOT NULL DEFAULT '0';
 
 /*!40000 ALTER TABLE `OVDM_CruiseDataTransfers` ENABLE KEYS */;
 UNLOCK TABLES;
@@ -30,8 +30,8 @@ UNLOCK TABLES;
 
 LOCK TABLES `OVDM_ExtraDirectories` WRITE;
 /*!40000 ALTER TABLE `OVDM_ExtraDirectories` DISABLE KEYS */;
-ALTER TABLE `OVDM_ExtraDirectories` ADD
-`cruiseOrLowering` int(1) unsigned NOT NULL DEFAULT '0';
+ALTER TABLE `OVDM_ExtraDirectories`
+ADD `cruiseOrLowering` int(1) unsigned NOT NULL DEFAULT '0';
 
 /*!40000 ALTER TABLE `OVDM_ExtraDirectories` ENABLE KEYS */;
 UNLOCK TABLES;
