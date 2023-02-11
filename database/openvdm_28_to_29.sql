0da19612637068ce7d3334409065de1f25a817dc
Webb Pinner <webbpinner@gmail.com>
2023-02-11 08:01:28 -0500

    reverting last commit

--- database/openvdm_28_to_29.sql
+++ database/openvdm_28_to_29.sql
@@ -10,3 +10,11 @@ INSERT INTO `OVDM_CoreVars` (`name`, `value`) VALUES
 
 /*!40000 ALTER TABLE `OVDM_CoreVars` ENABLE KEYS */;
 UNLOCK TABLES;
+
+LOCK TABLES `OVDM_CollectionSystemTransfers` WRITE;
+/*!40000 ALTER TABLE `OVDM_CollectionSystemTransfers` DISABLE KEYS */;
+ALTER TABLE `OVDM_CollectionSystemTransfers`
+ADD `removeSourceFiles` int(1) unsigned NOT NULL DEFAULT '0';
+
+/*!40000 ALTER TABLE `OVDM_CollectionSystemTransfers` ENABLE KEYS */;
+UNLOCK TABLES;
