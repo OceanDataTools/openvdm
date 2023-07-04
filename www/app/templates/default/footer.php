42466adefc7e489c9ed172b4b9808fb5418fe04f
Webb Pinner <webb.pinner@oceanxplorer.org>
2023-07-04 11:27:38 +0000

    trying to fix leaflet issues

--- www/app/templates/default/footer.php
+++ www/app/templates/default/footer.php
@@ -66,8 +66,8 @@ if (isset($data['javascript'])){
         if ($jsFile === 'leaflet') {
             array_push($jsFileArray, DIR . 'bower_components/leaflet/dist/leaflet.js');
             array_push($jsFileArray, DIR . 'bower_components/esri-leaflet/dist/esri-leaflet.js');
-            array_push($jsFileArray, DIR . 'bower_components/leaflet-fullscreen-bower/Leaflet.fullscreen.min.js');
-            array_push($jsFileArray, DIR . 'bower_components/leaflet-easyprint/dist/bundle.js');
+            array_push($jsFileArray, DIR . 'bower_components/leaflet-fullscreen/dist/Leaflet.fullscreen.min.js');
+            array_push($jsFileArray, DIR . 'node_modules/leaflet-easyprint/dist/bundle.js');
             //array_push($jsFileArray, Url::templatePath() . "js/esriCredit.js");
         } else if ($jsFile === 'leaflet-timedimension') {
             array_push($jsFileArray, DIR . 'bower_components/leaflet-timedimension/dist/leaflet.timedimension.min.js');
