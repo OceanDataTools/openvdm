2572ec79b333554619a21979c067cc979bf83bf1
Webb Pinner <webbpinner@gmail.com>
2025-01-27 11:08:43 -0400

    applied updated layout to ExtraDirectories page

--- www/app/views/Config/extraDirectories.php
+++ www/app/views/Config/extraDirectories.php
@@ -25,41 +25,50 @@ use Helpers\Session;
     </div>
     <div class="row">
         <div class="col-lg-7 col-md-12">
+            <div style="padding-bottom: 10px">
+                <a class="pull-right btn btn-sm btn-primary" href="<?php echo DIR; ?>config/extraDirectories/add">
+                    Add New Directory
+                </a>
+            </div>
             <table class='table table-striped table-hover table-bordered responsive'>
-                <tr>
-                    <th>Name</th>
-                    <th>Action</th>
-                    <th style='width:20px;'>Enabled</th>
-                </tr>
+                <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Action</th>
+                        <th style='width:20px;'>Enabled</th>
+                    </tr>
+                </thead>
+                </tbody>
 <?php
     if($data['extraDirectories']){
         foreach($data['extraDirectories'] as $row){
 ?>
-                <tr>
-                    <td><?php echo $row->longName; ?></td>
-                    <td>
-                        <a href='<?php echo DIR; ?>config/extraDirectories/edit/<?php echo $row->extraDirectoryID; ?>'>Edit</a>
-                        /
-                        <a href='#confirmDeleteModal' data-toggle="modal" data-item-name="Extra Directory" data-delete-url="<?php echo DIR; ?>config/extraDirectories/delete/<?php echo $row->extraDirectoryID; ?>">Delete</a>
-                    </td>
-                    <td style='text-align:center'>
+                    <tr>
+                        <td class="name"><?php echo $row->longName; ?></td>
+                        <td class="action">
+                            <a href='<?php echo DIR; ?>config/extraDirectories/edit/<?php echo $row->extraDirectoryID; ?>'>Edit</a>
+                            /
+                            <a href='#confirmDeleteModal' data-toggle="modal" data-item-name="Extra Directory" data-delete-url="<?php echo DIR; ?>config/extraDirectories/delete/<?php echo $row->extraDirectoryID; ?>">Delete</a>
+                        </td>
+                        <td style='text-align:center'>
 <?php
             if($row->enable == "0"){
 ?>
-                        <a class="btn btn-xs btn-danger" href='<?php echo DIR; ?>config/extraDirectories/enable/<?php echo $row->extraDirectoryID; ?>'>Off</a>
+                            <a class="btn btn-xs btn-danger" href='<?php echo DIR; ?>config/extraDirectories/enable/<?php echo $row->extraDirectoryID; ?>'>Off</a>
 <?php
             } else {
 ?>
-                        <a class="btn btn-xs btn-success" href='<?php echo DIR; ?>config/extraDirectories/disable/<?php echo $row->extraDirectoryID; ?>'>On</a>
+                            <a class="btn btn-xs btn-success" href='<?php echo DIR; ?>config/extraDirectories/disable/<?php echo $row->extraDirectoryID; ?>'>On</a>
 <?php
             }
 ?>                               
-                    </td>
-                </tr>
+                        </td>
+                    </tr>
 <?php
         }
     }
 ?>
+                </tbody>
             </table>
             <a class="btn btn-sm btn-primary" href="<?php echo DIR; ?>config/extraDirectories/add">Add New Extra Directory</a>
         </div>
