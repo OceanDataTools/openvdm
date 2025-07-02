8180a23e3171e1e7f1406f686d4c63a15f125408
Webb Pinner <webbpinner@gmail.com>
2025-07-02 11:05:15 -0400

    Relocates lowering directory definition
    Moves the definition of the lowering directory to after the retrieval of the shipboard data warehouse configuration.
    This ensures that the configuration is available before attempting to construct the lowering directory path.

--- server/workers/lowering.py
+++ server/workers/lowering.py
@@ -166,13 +166,9 @@ class OVDMGearmanWorker(python3_gearman.GearmanWorker): # pylint: disable=too-ma
         self.lowering_start_date = payload_obj.get('loweringStartDate', self.ovdm.get_lowering_start_date())
         self.lowering_end_date = payload_obj.get('loweringEndDate', self.ovdm.get_lowering_end_date())
 
-        logging.debug("loweringDataBaseDir: %s", self.shipboard_data_warehouse_config['loweringDataBaseDir'])
-        logging.debug("lowering_id: %s", self.lowering_id)
-
-        self.lowering_dir = os.path.join(self.shipboard_data_warehouse_config['loweringDataBaseDir'], self.lowering_id)
-
         self.shipboard_data_warehouse_config = self.ovdm.get_shipboard_data_warehouse_config()
         self.cruise_dir = os.path.join(self.shipboard_data_warehouse_config['shipboardDataWarehouseBaseDir'], self.cruise_id)
+        self.lowering_dir = os.path.join(self.shipboard_data_warehouse_config['loweringDataBaseDir'], self.lowering_id)
 
         return super().on_job_execute(current_job)
 
