72594895c716f2cde4948b16c0b9c6479db5a8cb
Webb Pinner <webbpinner@gmail.com>
2026-06-23 08:47:04 -0400

    Set SELinux context for OpenVDM log directory

--- utils/install-openvdm.sh
+++ utils/install-openvdm.sh
@@ -1220,6 +1220,7 @@ EOF
 
         echo "Setting SELinux exception rules"
         chcon -R -t httpd_sys_content_t ${DATA_ROOT} 2>/dev/null || true
+        chcon -R -t httpd_sys_content_t /var/log/openvdm 2>/dev/null || true
         chcon -R -t httpd_sys_rw_content_t /var/www/openvdm/errorlog.html 2>/dev/null || true
 
         setsebool httpd_tmp_exec on
